3cb0504c5128214bd704afa1146460a014786b45
comatoki <hbabnd@gmail.com>
2024-06-03 19:42:57 +0900

    Toast Message Implementation

---
+++ ToastMessageTest/ToastMessage.swift
@@ -0,0 +1,18 @@
+//
+//  ToastMessage.swift
+//  ToastMessageTest
+//
+//  Created by COMATOKI on 2024-06-03.
+//
+
+import SwiftUI
+
+struct ToastMessage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToastMessage()
+}
