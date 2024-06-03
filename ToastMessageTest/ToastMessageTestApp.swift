db0dc5c9f71af0ec310b25f262951e67da89330b
comatoki <hbabnd@gmail.com>
2024-06-03 19:21:04 +0900

    Initial Commit

---
+++ ToastMessageTest/ToastMessageTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastMessageTestApp.swift
+//  ToastMessageTest
+//
+//  Created by COMATOKI on 2024-06-03.
+//
+
+import SwiftUI
+
+@main
+struct ToastMessageTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
